ce5a44756115aa7990304b5c7586e7220e0fc77e
bhtran <bhtran00@gmail.com>
2016-06-17 18:46:12 -0400

    Orientation size logic

---
+++ Cat Map/AddressViewController.h
@@ -0,0 +1,14 @@
+//
+//  AddressViewController.h
+//  Cat Map
+//
+//  Created by Bao Tran on 6/17/16.
+//  Copyright © 2016 Bao Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ViewController.h"
+
+@interface AddressViewController : UIViewController
+
+@end
